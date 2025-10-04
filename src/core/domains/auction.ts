a446a201a3d82a6594db42f6d4bfb3a886794a37
siegfriedbz <siegfried.bozza@yahoo.com>
2025-10-04 16:52:28 +0200

    feat(core): implement domains and mappers for auctions, bids, and users

--- src/core/domains/auction.ts
+++ src/core/domains/auction.ts
@@ -1,42 +1,129 @@
-import type { User } from "./user";
+import { z } from "zod";
+import { UserSchema } from "./user";
 
-export enum AuctionStatus {
-  DRAFT = "DRAFT", // Not visible to bidders yet
-  OPEN = "OPEN",
-  CLOSED = "CLOSED",
-  CANCELLED = "CANCELLED",
-}
+// enums
+export const AuctionCategorySchema = z.enum([
+  "ELECTRONICS",
+  "FASHION",
+  "COLLECTIBLES",
+  "ART",
+  "MUSIC",
+  "SPORTS",
+  "HOME",
+  "TOYS",
+  "AUTOMOTIVE",
+]);
+export type AuctionCategory = z.infer<typeof AuctionCategorySchema>;
 
-export enum Category {
-  ELECTRONICS = "Electronics",
-  FASHION = "Fashion",
-  COLLECTIBLES = "Collectibles",
-  ART = "Art",
-  MUSIC = "Music",
-  SPORTS = "Sports",
-  HOME = "Home",
-  TOYS = "Toys",
-  AUTOMOTIVE = "Automotive",
-}
+export const AuctionStatusSchema = z.enum([
+  "DRAFT",
+  "OPEN",
+  "CLOSED",
+  "CANCELLED",
+]);
+export type AuctionStatus = z.infer<typeof AuctionStatusSchema>;
 
-export type AuctionImage = {
-  url: string; // public URL from Supabase Storage
-  name?: string;
-  uploadedAt?: Date;
+// img
+export const AuctionImageSchema = z.object({
+  url: z.url(),
+  name: z.string().optional(),
+  uploadedAt: z.date().optional(),
+});
+
+export type AuctionImage = z.infer<typeof AuctionImageSchema>;
+
+// Auction
+export const AuctionSchema = z.object({
+  id: z.uuid(),
+  ownerId: z.uuid(),
+  title: z.string(),
+  description: z.string(),
+  images: z.array(AuctionImageSchema),
+  category: AuctionCategorySchema,
+  startingPrice: z
+    .number()
+    .min(1, { message: "Starting price must be greater than 1" }),
+  currentBid: z.number().optional(),
+  status: AuctionStatusSchema,
+  startedAt: z.date().optional(),
+  endAt: z.date().optional(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+});
+
+export type Auction = z.infer<typeof AuctionSchema>;
+
+// AuctionDetailsSchema for auction details page
+export const AuctionDetailsSchema = AuctionSchema.extend({
+  owner: UserSchema.omit({
+    role: true,
+    createdAt: true,
+    updatedAt: true,
+  }).nullable(),
+});
+
+export type AuctionDetails = z.infer<typeof AuctionDetailsSchema>;
+
+export type AuctionsListingParams = {
+  filterBy?: Partial<Pick<Auction, "title" | "status" | "category">>;
+  orderBy?: {
+    key: AuctionsSortKey;
+    order: AuctionsSortOrder;
+  };
+  pagination?: {
+    page: number;
+    size: number;
+  };
 };
 
-export type Auction = {
-  id: string;
-  ownerId: User["id"];
-  title: string;
-  description: string;
-  images: AuctionImage[];
-  category: Category;
-  startingPrice: number;
-  currentBid?: number;
-  status: AuctionStatus;
-  startedAt?: Date | string;
-  endAt?: Date | string;
-  createdAt?: Date | string;
-  updatedAt?: Date | string;
+export type AuctionsCountParams = Omit<AuctionsListingParams, "pagination">;
+
+// create & update params - FE
+const BaseAuctionFields = AuctionSchema.pick({
+  title: true,
+  description: true,
+  category: true,
+  startingPrice: true,
+});
+
+const BaseAuctionUpdateFields = AuctionSchema.pick({ id: true }).extend(
+  BaseAuctionFields.shape,
+);
+
+// create auction - FE params
+export const CreateAuctionStatusSchema = z.enum([
+  AuctionStatusSchema.enum.DRAFT,
+  AuctionStatusSchema.enum.OPEN,
+]);
+export const CreateAuctionParamsSchema = BaseAuctionFields.extend({
+  endAt: AuctionSchema.shape.endAt.optional(),
+  status: CreateAuctionStatusSchema,
+  images: z.array(AuctionImageSchema).optional(),
+});
+export type CreateAuctionParams = z.infer<typeof CreateAuctionParamsSchema>;
+
+// update auction - FE params
+export const UpdateAuctionStatusSchema = AuctionStatusSchema;
+export const UpdateAuctionParamsSchema = BaseAuctionUpdateFields.extend({
+  endAt: AuctionSchema.shape.endAt.optional(),
+  status: UpdateAuctionStatusSchema,
+  images: z.array(AuctionImageSchema).optional(),
+});
+export type UpdateAuctionParams = z.infer<typeof UpdateAuctionParamsSchema>;
+
+// Sorting
+export const SORTABLE_KEYS = [
+  "category",
+  "status",
+  "startingPrice",
+  "createdAt",
+] as const;
+export const AuctionsSortKeySchema = z.enum(SORTABLE_KEYS);
+export type AuctionsSortKey = z.infer<typeof AuctionsSortKeySchema>;
+export const AuctionsSortOrderSchema = z.enum(["asc", "desc"]);
+export type AuctionsSortOrder = z.infer<typeof AuctionsSortOrderSchema>;
+
+export const ORDER_COLUMN_MAP: Record<string, string> = {
+  createdAt: "created_at",
+  startingPrice: "starting_price",
 };
---
+++ src/core/infra/supabase/auction-mapper.ts
@@ -0,0 +1,63 @@
+import z from "zod";
+import {
+  type Auction,
+  type AuctionImage,
+  AuctionSchema,
+} from "@/core/domains/auction";
+
+export type SupabaseAuctionImage = Pick<AuctionImage, "url" | "name"> & {
+  uploaded_at?: string;
+};
+
+export type SupabaseAuction = Pick<
+  Auction,
+  "id" | "title" | "description" | "category" | "status"
+> & {
+  owner_id: string;
+  images: SupabaseAuctionImage[];
+  starting_price: number;
+  current_bid?: number;
+  started_at?: string;
+  end_at?: string;
+  created_at: string;
+  updated_at: string;
+};
+
+export const auctionMapper = (row: SupabaseAuction | null) => {
+  if (!row) return null;
+
+  const normalized = normalizeAuctionData(row);
+
+  const result = AuctionSchema.safeParse(normalized);
+  if (!result.success) {
+    console.warn("Invalid auction data", z.treeifyError(result.error));
+    return null;
+  }
+
+  return result.data;
+};
+
+export const normalizeAuctionData = (row: SupabaseAuction) => {
+  // TODO FIX
+  // const normalizedImages: AuctionImage[] = row.images?.map((img) => ({
+  //   url: img.url,
+  //   name: img.name ?? undefined,
+  //   uploadedAt: img.uploaded_at ? new Date(img.uploaded_at) : undefined,
+  // }));
+
+  return {
+    id: row.id,
+    ownerId: row.owner_id,
+    title: row.title,
+    description: row.description,
+    images: [{ url: "https://github.com/shadcn.png" }],
+    category: row.category,
+    startingPrice: row.starting_price,
+    currentBid: row.current_bid ?? undefined,
+    status: row.status,
+    startedAt: row.started_at ? new Date(row.started_at) : undefined,
+    endAt: row.end_at ? new Date(row.end_at) : undefined,
+    createdAt: new Date(row.created_at),
+    updatedAt: new Date(row.updated_at),
+  };
+};
