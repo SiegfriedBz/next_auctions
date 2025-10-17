8e742c34b65c40abdce9cdf8be78f0c67b0798b7
siegfriedbz <siegfried.bozza@yahoo.com>
2025-10-17 23:11:40 +0200

    feat(stripe): add server action and button to create checkout session

---
+++ src/actions/stripe/create-checkout-session.ts
@@ -0,0 +1,57 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import Stripe from "stripe";
+import { auctions } from "@/core/instances/auctions";
+import { users } from "@/core/instances/users";
+import type { LangParam } from "@/i18n";
+
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY ?? "";
+const NEXT_PUBLIC_APP_URL = process.env.NEXT_PUBLIC_APP_URL ?? "";
+
+type Params = {
+  auctionId: string;
+} & LangParam;
+
+export const createCheckoutSession = async (params: Params): Promise<void> => {
+  const { auctionId, lang } = params;
+
+  const [user, auction] = await Promise.all([
+    users().me(),
+    auctions().detailsById(auctionId),
+  ]);
+
+  if (!user) throw new Error("User not found");
+  if (!auction) throw new Error("Auction not found");
+  if (!auction.highestBid) throw new Error("Auction bid value not found");
+
+  const stripe = new Stripe(STRIPE_SECRET_KEY);
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    mode: "payment",
+    customer_email: user?.email,
+    line_items: [
+      {
+        price_data: {
+          currency: "usd", // or auction.currency
+          product_data: {
+            name: `Auction ${auction.title}`,
+          },
+          unit_amount: auction.highestBid * 100, // in cents
+        },
+        quantity: 1,
+      },
+    ],
+
+    success_url: `${NEXT_PUBLIC_APP_URL}/${lang}/auctions/${auctionId}/stripe/success?sessionId={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${NEXT_PUBLIC_APP_URL}/${lang}/auctions/${auctionId}/stripe/cancel`,
+  });
+
+  const sessionUrl = session.url;
+  if (!sessionUrl) {
+    throw new Error("sessionUrl was not created");
+  }
+
+  redirect(sessionUrl);
+};
--- src/app/[lang]/(authenticated)/auctions/_components/initiate-payment.button.tsx
+++ src/app/[lang]/(authenticated)/auctions/_components/initiate-payment.button.tsx
@@ -1,12 +1,29 @@
 "use client";
 
 import { Trans } from "@lingui/react/macro";
-import type { FC } from "react";
+import { type FC, useCallback } from "react";
+import { createCheckoutSession } from "@/actions/stripe/create-checkout-session";
 import { Button } from "@/components/ui/button";
+import type { LangParam } from "@/i18n";
 
-export const InitiatePaymentButton: FC = () => {
+type Props = {
+  auctionId: string;
+} & LangParam;
+
+export const InitiatePaymentButton: FC<Props> = (props) => {
+  const { auctionId, lang } = props;
+
+  const onCreateCheckout = useCallback(async () => {
+    await createCheckoutSession({ auctionId, lang });
+  }, [auctionId, lang]);
+
   return (
-    <Button size="lg" className="cursor-pointer">
+    <Button
+      onClick={onCreateCheckout}
+      type="button"
+      size="lg"
+      className="cursor-pointer"
+    >
       <Trans>You won this auction</Trans>
     </Button>
   );
