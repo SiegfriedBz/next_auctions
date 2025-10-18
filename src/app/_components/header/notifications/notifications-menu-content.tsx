df24aabc4030ca119adee4748bcbfd980ad2dc29
siegfriedbz <siegfried.bozza@yahoo.com>
2025-10-18 13:56:43 +0200

    feat(ui): update notifications menu tabs

--- src/app/_components/header/notifications/notifications-menu-content.tsx
+++ src/app/_components/header/notifications/notifications-menu-content.tsx
@@ -2,13 +2,15 @@
 
 import { Trans, useLingui } from "@lingui/react/macro";
 import { BellIcon, BellRingIcon } from "lucide-react";
-import { type FC, useMemo } from "react";
+import { type FC, memo, useMemo } from "react";
 import { NavigationMenuLink } from "@/components/ui/navigation-menu";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   type Notification,
   NotificationTypeSchema,
+  type NotificationTypeSchemaT,
 } from "@/core/domains/notifications";
+import { cn } from "@/lib/utils";
 import { NotificationItem } from "../../real-time/notification-item";
 
 type Props = {
@@ -22,20 +24,48 @@ export const NotificationMenuContent: FC<Props> = (props) => {
 
   const { t } = useLingui();
 
-  const newBidsNotifications = useMemo(() => {
-    return notifications.filter(
-      (n) => n.type === NotificationTypeSchema.enum.NEW_BID,
+  const notificationsByType = useMemo(() => {
+    return notifications.reduce<
+      Record<NotificationTypeSchemaT, Notification[]>
+    >(
+      (acc, n) => {
+        acc[n.type] = acc[n.type] || [];
+        acc[n.type].push(n);
+        return acc;
+      },
+      {} as Record<NotificationTypeSchemaT, Notification[]>,
     );
   }, [notifications]);
 
-  const newAuctionsWonNotifications = useMemo(() => {
-    return notifications.filter(
-      (n) => n.type === NotificationTypeSchema.enum.NEW_AUCTION_WON,
-    );
-  }, [notifications]);
+  const tabsList = useMemo(() => {
+    return [
+      {
+        value: "payments",
+        label: t`Payments`,
+        defaultLabel: t`No payment`,
+        notifications:
+          notificationsByType[NotificationTypeSchema.enum.NEW_PAYMENT] || [],
+      },
+      {
+        value: "bids",
+        label: t`Bids`,
+        defaultLabel: t`No bid`,
+        notifications:
+          notificationsByType[NotificationTypeSchema.enum.NEW_BID] || [],
+      },
+      {
+        value: "auctions-won",
+        label: t`Auctions won`,
+        defaultLabel: t`No auction won`,
+        notifications:
+          notificationsByType[NotificationTypeSchema.enum.NEW_AUCTION_WON] ||
+          [],
+      },
+    ];
+  }, [notificationsByType, t]);
 
   return (
-    <Tabs defaultValue="bids" className="w-full px-1">
+    <Tabs defaultValue="payments" className="w-full px-1">
       <div className="flex justify-center items-center gap-x-2 mt-2 mb-1">
         {count > 0 ? (
           <BellRingIcon className="size-4 opacity-90" />
@@ -46,28 +76,33 @@ export const NotificationMenuContent: FC<Props> = (props) => {
       </div>
 
       <TabsList className="w-full">
-        <TabsTrigger value="bids" className="cursor-pointer">
-          <Trans>Bids</Trans>
-        </TabsTrigger>
-        <TabsTrigger value="auctions-won" className="cursor-pointer">
-          <Trans>Auctions won</Trans>
-        </TabsTrigger>
+        {tabsList.map((tab) => {
+          return (
+            <TabsTrigger
+              key={tab.value}
+              value={tab.value}
+              className={cn("cursor-pointer opacity-80", {
+                "opacity-100": tab.notifications.length > 0,
+              })}
+              aria-selected={tab.notifications.length > 0}
+            >
+              {tab.label}
+            </TabsTrigger>
+          );
+        })}
       </TabsList>
 
-      <TabsContent value="bids">
-        <TabContentInner
-          notifications={newBidsNotifications}
-          label={t`No new bid`}
-          onMarkNotificationAsSeen={onMarkNotificationAsSeen}
-        />
-      </TabsContent>
-      <TabsContent value="auctions-won">
-        <TabContentInner
-          notifications={newAuctionsWonNotifications}
-          label={t`No new auction won`}
-          onMarkNotificationAsSeen={onMarkNotificationAsSeen}
-        />
-      </TabsContent>
+      {tabsList.map((tab) => {
+        return (
+          <TabsContent key={`content-${tab.value}`} value={tab.value}>
+            <MemoTabContentInner
+              notifications={tab.notifications}
+              label={tab.defaultLabel}
+              onMarkNotificationAsSeen={onMarkNotificationAsSeen}
+            />
+          </TabsContent>
+        );
+      })}
     </Tabs>
   );
 };
@@ -105,3 +140,5 @@ const TabContentInner: FC<TabContentInnerProps> = (props) => {
     </ul>
   );
 };
+
+const MemoTabContentInner = memo(TabContentInner);
