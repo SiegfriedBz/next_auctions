cc12779c8e78a960840270bb68a0d4564495c67c
siegfriedbz <siegfried.bozza@yahoo.com>
2025-10-19 20:21:29 +0200

    [Clean-up] Remove obsolete profile components and update typography

--- src/app/[lang]/(group)/profile/edit/page.tsx
+++ src/app/[lang]/(group)/profile/edit/page.tsx
@@ -1,7 +1,8 @@
 import { Trans } from "@lingui/react/macro";
 import { redirect } from "next/navigation";
-import { type FC, Suspense } from "react";
-import { SkeletonProfileForm } from "@/app/[lang]/(group)/profile/edit/_components/skeleton-profile-form";
+import type { FC } from "react";
+import { TypographyH2 } from "@/app/_components/typography/h2";
+import { TypographyLead } from "@/app/_components/typography/lead";
 import {
   Card,
   CardContent,
@@ -11,7 +12,7 @@ import {
 } from "@/components/ui/card";
 import { users } from "@/core/instances/users";
 import { type LangParam, withI18n } from "@/i18n";
-import { UpdateUserFormServer } from "./_components/update-profile-form-server";
+import { UpdateProfileForm } from "./_components/update-profile-form";
 
 type Props = {
   params: Promise<LangParam>;
@@ -25,22 +26,30 @@ const Page: FC<Props> = async (props) => {
     redirect(`/${lang}`);
   }
 
+  const defaultValues = {
+    firstName: me.firstName,
+    lastName: me.lastName,
+    avatarUrl: me.avatarUrl,
+  };
+
   return (
     <div className="max-w-2xl flex flex-col gap-4 mx-auto pb-4">
       <Card>
         <CardHeader>
-          <CardTitle className="h2">
-            <Trans>Update your profile</Trans>
+          <CardTitle>
+            <TypographyH2>
+              <Trans>Update your profile</Trans>
+            </TypographyH2>
           </CardTitle>
           <CardDescription>
-            <Trans>Fill the form below to update your profile</Trans>
+            <TypographyLead>
+              <Trans>Fill the form below to update your profile</Trans>
+            </TypographyLead>
           </CardDescription>
         </CardHeader>
 
         <CardContent>
-          <Suspense fallback={<SkeletonProfileForm />}>
-            <UpdateUserFormServer />
-          </Suspense>
+          <UpdateProfileForm defaultValues={defaultValues} userId={me.id} />
         </CardContent>
       </Card>
     </div>
